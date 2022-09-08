4fce8f8b9aeb7e8237ac345fd0be991d3aa12b38
x54x6fx6d <tom31schule@web.de>
2022-09-08 20:47:59 +0200

    base

---
+++ pages/Blog.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function Blog({ Component, pageProps }: AppProps) {
+  return 
+  <div>
+
+  </div>
+}
+
+export default Blog
