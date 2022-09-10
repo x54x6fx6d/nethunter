75b943f8470bbc7bdd1e29a6d2ec594b6044d1b5
x54x6fx6d <tom31schule@web.de>
2022-09-10 04:08:25 +0200

    added logo and animation (in hover)

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,8 +1,24 @@
 import styles from "../styles/shop.module.css";
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Image from 'next/image';
+import mypic from "./api/assets/lotusTransparent.png";
 
 function shop({Component, pageProps}: AppProps){
     return <div className={styles.wrapper}>
+        <div
+        style={{
+            width: "200px",
+            height: "200px",
+            position: "absolute",
+            left: "25px",
+        }}
+        >
+        <Image className={styles.logoWrapper}
+        src={mypic}
+        width={100}
+        height={100}
+        />
+        </div>
         <div className={styles.navbar}>
             <ul>
                 <li><a href="/home">Home</a></li>
