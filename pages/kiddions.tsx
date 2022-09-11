454a3373a5aacd8b3e10a27bf5b2fea5fd592521
x54x6fx6d <tom31schule@web.de>
2022-09-11 15:53:37 +0200

    kiddions

--- pages/kiddions.tsx
+++ pages/kiddions.tsx
@@ -1,10 +1,13 @@
 import styles from "../styles/shopItems/kiddions.module.css";
 import type { AppProps } from 'next/app';
 import Image from 'next/image';
+import kiddionsImage from "./api/assets/kiddionsImage.png";
 
 function kiddions({Component, pageProps}: AppProps) {
     return <div className={styles.wrapper}>
-        <p>Test</p>
+        <Image
+        src={kiddionsImage}
+        ></Image>
     </div>
 }
 
